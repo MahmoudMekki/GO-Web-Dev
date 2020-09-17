b3deb2cdbff4559978697aa330bde57979cebe4f
MahmoudMekki <mahmoudmekki3@gmail.com>
2020-09-17 18:29:20 +0200

    passing through forms

---
+++ passing-data/forms/01/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon/", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func foo(res http.ResponseWriter, req *http.Request) {
+	v := req.FormValue("q")
+	res.Header().Set("Content-Type", "text/html;charset=utf-8")
+	io.WriteString(res, `
+	<form method="get">
+	<input type="text" name = "q">
+	<input type="submit">
+	</form>
+	<br>`+v)
+}
